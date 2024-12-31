640df6059908ba67ae63dd14f6439017343a9375
Script-Savant <njugunakinuthia013@gmail.com>
2024-12-31 14:28:30 +0300

    working with lists

--- NumbersInCSharp/Program.cs
+++ NumbersInCSharp/Program.cs
@@ -22,10 +22,48 @@
 // }
 
 // for loop
-for (int i = 0; i < 5; i++) {
-    if (i == 3) {
-        Console.WriteLine("Three");
-    } else {
-        Console.WriteLine(i);
-    }
+// for (int i = 0; i < 5; i++) {
+//     if (i == 3) {
+//         Console.WriteLine("Three");
+//     } else {
+//         Console.WriteLine(i);
+//     }
+// }
+
+// List T Collections of Data
+var names = new List<string> {"Felipe", "James", "Monroe"};
+
+names.Add("Corazon");
+names.Add("Ernesto");
+names.Add("Christian");
+
+// Sort names
+names.Sort();
+// Reverse names
+names.Reverse();
+// Find the index of
+Console.WriteLine($"Index of Corazon is {names.IndexOf("Corazon")}");
+
+foreach (var name in names) 
+{
+    Console.WriteLine(name);
 }
+
+// Console.WriteLine(names[0]);
+// Console.WriteLine(names[names.Count-1]); // Use count to get the last element of the list
+// Console.WriteLine(names[^2]); // count from the back of the list
+
+// Console.WriteLine();
+
+// foreach (var name in names[2..4]){ // get a range from the list
+//     Console.WriteLine(name);
+// }
+
+// var newNames = new string[] { "Edith", "Casemiro", "Mondragon" };
+// // Add to an array
+// newNames = [.. newNames, "Kairo"];
+// foreach (var name in newNames)
+// {
+//     Console.WriteLine(name);
+// }
+
--- NumbersInCSharp/obj/Debug/net9.0/NumbersInCSharp.AssemblyInfo.cs
+++ NumbersInCSharp/obj/Debug/net9.0/NumbersInCSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NumbersInCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f04275c5ed346596d6140105c155fd00d6c8f694")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df935d6d73626165c3960dbec6a13d4ad5f4337f")]
 [assembly: System.Reflection.AssemblyProductAttribute("NumbersInCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NumbersInCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
