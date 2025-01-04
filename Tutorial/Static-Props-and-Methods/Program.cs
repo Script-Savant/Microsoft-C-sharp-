c1496a2637f83779fe3f6f1f6234a62b2fd6a0c1
Script-Savant <njugunakinuthia013@gmail.com>
2025-01-04 23:25:00 +0300

    static properties

---
+++ Tutorial/Static-Props-and-Methods/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Person
+{
+    public static int PopulationCounter { get; set; } = 0;
+    public string Name { get; set; }
+
+    public Person(string name)
+    {
+        Name = name;
+        PopulationCounter++;
+    }
+}
+
+internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Person p1 = new("Saldina");
+        Person p2 = new("Joe");
+        Person p3 = new("Emiliano");
+
+        Console.WriteLine(p1.Name);
+        Console.WriteLine(Person.PopulationCounter);
+    }
+}
