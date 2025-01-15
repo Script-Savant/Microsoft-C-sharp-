127af00812d34b85f92de334b1dd829aaa619835
Alex Kinuthia <njugunakinuthia013@gmail.com>
2025-01-15 20:00:26 +0300

    database migrations and seed data

---
+++ Projects/Restaurant/Restaurant/Data/Migrations/20250115164849_AddSeedData.cs
@@ -0,0 +1,204 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Restaurant.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Appetizer" },
+                    { 2, "Entree" },
+                    { 3, "Side Dish" },
+                    { 4, "Dessert" },
+                    { 5, "Beverage" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Ingredients",
+                columns: new[] { "IngredientId", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Beef" },
+                    { 2, "Chicken" },
+                    { 3, "Fish" },
+                    { 4, "Tortilla" },
+                    { 5, "Lettuce" },
+                    { 6, "Tomato" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "CategoryId", "Description", "Name", "Price", "Stock" },
+                values: new object[,]
+                {
+                    { 1, 2, "A delcious beef taco", "Beef Taco", 2.50m, 100 },
+                    { 2, 2, "A delcious chicken taco", "Chicken Taco", 1.99m, 90 },
+                    { 3, 2, "A delcious fish taco", "Fish Taco", 2.00m, 101 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ProductIngredients",
+                columns: new[] { "IngredientId", "ProductId" },
+                values: new object[,]
+                {
+                    { 1, 1 },
+                    { 4, 1 },
+                    { 5, 1 },
+                    { 6, 1 },
+                    { 2, 2 },
+                    { 4, 2 },
+                    { 5, 2 },
+                    { 6, 2 },
+                    { 3, 3 },
+                    { 4, 3 },
+                    { 5, 3 },
+                    { 6, 3 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "ProductIngredients",
+                keyColumns: new[] { "IngredientId", "ProductId" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "ProductIngredients",
+                keyColumns: new[] { "IngredientId", "ProductId" },
+                keyValues: new object[] { 4, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "ProductIngredients",
+                keyColumns: new[] { "IngredientId", "ProductId" },
+                keyValues: new object[] { 5, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "ProductIngredients",
+                keyColumns: new[] { "IngredientId", "ProductId" },
+                keyValues: new object[] { 6, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "ProductIngredients",
+                keyColumns: new[] { "IngredientId", "ProductId" },
+                keyValues: new object[] { 2, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "ProductIngredients",
+                keyColumns: new[] { "IngredientId", "ProductId" },
+                keyValues: new object[] { 4, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "ProductIngredients",
+                keyColumns: new[] { "IngredientId", "ProductId" },
+                keyValues: new object[] { 5, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "ProductIngredients",
+                keyColumns: new[] { "IngredientId", "ProductId" },
+                keyValues: new object[] { 6, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "ProductIngredients",
+                keyColumns: new[] { "IngredientId", "ProductId" },
+                keyValues: new object[] { 3, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "ProductIngredients",
+                keyColumns: new[] { "IngredientId", "ProductId" },
+                keyValues: new object[] { 4, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "ProductIngredients",
+                keyColumns: new[] { "IngredientId", "ProductId" },
+                keyValues: new object[] { 5, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "ProductIngredients",
+                keyColumns: new[] { "IngredientId", "ProductId" },
+                keyValues: new object[] { 6, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "Ingredients",
+                keyColumn: "IngredientId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Ingredients",
+                keyColumn: "IngredientId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Ingredients",
+                keyColumn: "IngredientId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Ingredients",
+                keyColumn: "IngredientId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Ingredients",
+                keyColumn: "IngredientId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Ingredients",
+                keyColumn: "IngredientId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 2);
+        }
+    }
+}
