8aa8f3f02807026e751b6e595ac816b8dae4a0e2
Alex Kinuthia <njugunakinuthia013@gmail.com>
2025-01-18 23:45:27 +0300

    init

---
+++ Projects/EmployeesManagement/EmployeesManagement/Models/UserActivity.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeesManagement.Models
+{
+    public class UserActivity
+    {
+        public string? CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string? ModifiedBy { get; set; }
+        public DateTime ModifiedOn { get; set; }
+    }
+}
