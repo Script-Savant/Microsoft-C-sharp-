bc64b7b48ef214163730f534f8f957d3bac0ebb0
Alex Kinuthia <njugunakinuthia013@gmail.com>
2025-02-05 00:35:08 +0300

    party invites init

---
+++ Projects/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+
+
+namespace PartyInvites.Controllers;
+
+public class HomeController : Controller
+{
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult RsvpForm()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult RsvpForm([Bind("Name, Email, Phone, WillAttend")] GuestResponse guestResponse)
+    {
+        Repository.AddResponse(guestResponse);
+        return View("Thanks", guestResponse);
+    }
+}
