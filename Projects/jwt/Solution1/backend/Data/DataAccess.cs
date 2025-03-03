f810b2d0f88119b19021a656e0e6c291f2aa7788
Script-Savant <alexkienjeku@gmail.com>
2025-03-03 14:45:02 +0300

    setup data access scenario for register user

---
+++ Projects/jwt/Solution1/backend/Data/DataAccess.cs
@@ -0,0 +1,35 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace backend.Data
+{
+    public class DataAccess : IDisposable
+    {
+        private SqlConnection connection;
+        public DataAccess(IConfiguration configuration) { 
+            string connectionString = configuration.GetConnectionString("DefaultConnection");
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+        }
+
+        public void Dispose()
+        {
+            if (connection != null)
+            {
+                connection.Dispose();
+                connection.Close();
+            }
+        }
+
+        public bool RegisterUser(string email, string password, string role)
+        {
+            var accountCount = connection.ExecuteScalar<int>("SELECT COUNT(1) FROM [UserAccount] WHERE [Email] = @email", new { email = email });
+            if (accountCount > 0) return false;
+
+            var query = "INSERT INTO [UserAccount] (Email, Password, Role) VALUES (@email, @password, @role)";
+            var result = connection.Execute(query, new { email = email, password = password, role = role });
+
+            return result > 0;
+        }
+    }
+}
