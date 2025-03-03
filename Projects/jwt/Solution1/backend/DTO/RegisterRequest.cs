75e84e2f416dd9ade6978efdb4efbd0aaa01374d
Script-Savant <alexkienjeku@gmail.com>
2025-03-03 14:45:45 +0300

    register user

---
+++ Projects/jwt/Solution1/backend/DTO/RegisterRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.DTO
+{
+    public class RegisterRequest
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string Role { get; set; }
+    }
+}
