cd5a6e22310d69a548e1952f3545e5fb33e31f25
Script-Savant <alexkienjeku@gmail.com>
2025-03-03 14:46:44 +0300

    endpoint t o create a new user "api/auth/register"

---
+++ Projects/jwt/Solution1/backend/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using backend.Data;
+using backend.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly DataAccess dataAccess;
+
+        public AuthController(DataAccess dataAccess)
+        {
+            this.dataAccess = dataAccess;
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register(RegisterRequest request)
+        {
+            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(request.Password);
+            var result = dataAccess.RegisterUser(request.Email, hashedPassword, request.Role);
+
+            return result ? Ok() : BadRequest();
+        }
+    }
+}
